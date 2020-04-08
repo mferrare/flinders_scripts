eaab2aca05bba62c3cbe0e4d3f62e88824a16c24
Mark Ferraretto <mark@ferraretto.com>
2020-04-08 11:16:30 +0930

    More functional package uploader + github mgmt

--- docassemble_upload_package.py
+++ docassemble_upload_package.py
@@ -5,15 +5,23 @@ import requests, json
 import os
 from os import listdir
 from os.path import isfile, join
+import argparse
+
+# Global variable - store secrets and other config info in here.
+config_data = {}
+
+# Default project in the playground
+project_name = "C3"
+# List of folders to iterate through.  This list comes from the
+# API documentation: https://docassemble.org/docs/api.html#playground_get
+project_folders = [ 'questions', 'sources', 'static', 'templates', 'modules']
 
 # Sends one file to the playground
 def MJFpush_to_playground(MJFpayload):
-    # Construct the payload
-    payload = {
-        'key'        : MJFpayload['api_key'],
-        'folder'     : MJFpayload['folder'],
-        'project'    : MJFpayload['project']
-    }
+
+    # Initialise    
+    construct_API_payload(MJFpayload)
+
     # create the list of multiple files
     file_payload = []
     for file in MJFpayload['files']:
@@ -21,59 +29,146 @@ def MJFpush_to_playground(MJFpayload):
             ('file', open(file, 'rb'))
         )
     # Send the file
-    response = requests.post(MJFpayload['URL'], data=payload, files=file_payload)
+    response = requests.post(MJFpayload['URL'], data=MJFpayload, files=file_payload)
     return response
+
+# Lists files in the playground
+def MJFlist_playground_files(MJFpayload):
+
+    # Add API key etc to payload
+    construct_API_payload(MJFpayload)
+
+    # Make the request
+    response = requests.get(MJFpayload['URL'], params=MJFpayload)
+
+    # If the response code is 200 we're OK.  In which case return the 
+    # list of files
+    if response.ok:
+        return response.json()
+    else:
+        return []
+
+def MJFdelete_playground_file(MJFpayload):
+    # Add API key, endpoint etc to payload
+    construct_API_payload(MJFpayload)
+    # Make the request
+    response = requests.delete(MJFpayload['URL'], params=MJFpayload)
+    if response.ok:
+        return True
+    else:
+        return False
+        
+
 
 # List all the files in the specified directory
 def MJFlist_files_in_dir(MJFdir):
-    return [join(MJFdir, f) for f in listdir(MJFdir) if isfile(join(MJFdir, f))]
+    try: 
+        return [join(MJFdir, f) for f in listdir(MJFdir) if isfile(join(MJFdir, f))]
+    except:
+        # Return nothing if the directory doesn't exist
+        return []
 
-
-# Main Program
-def main():
-
+# Get API key
+# Expect: Nothing
+# Return: Nothing
+# API key is stored in a secrets file which is HARD coded in this
+# sub.  Extract it and store it in config data global variable.
+def get_API_key():
     # Secrets file
     userprofile = os.environ['USERPROFILE']
     secrets_file = userprofile + '/OneDrive - Mark Ferraretto/kdb/secrets.json'
 
     # Read the secrets in from the secrets file
     with open(secrets_file, 'r') as fp:
-        secrets = fp.read()
+        secrets = json.loads(fp.read())
     fp.close()
-    secrets_j = json.loads(secrets)
+    config_data['api_key'] = secrets['flinders_api_unrestricted']
 
-    # Read in config file
-    with open('docassemble_notify_users.json', 'r') as fp:
-        config_text = fp.read()
-    config = json.loads(config_text)
+# Get the API URL and store it in config_data
+# Expect: Nothing
+# Return: Nothing
+# This was reading from a config file before but now it is
+# HARD CODED in here.
+# TODO: Should this get moved to a config file?
+def get_API_URL():
+    config_data['api_root'] = 'https://docassemble.flinders.edu.au/api'
 
-    # Store config variables somewhere easier to read
-    api_root = config['api_root']
-    api_key  = secrets_j['flinders_api_key']
+# construct_API_payload
+# Expect: API endpoint, dictionary with parameters to pass through to API
+# Return: payload dictionary
+# Constructs payload to pass to API.  
+def construct_API_payload(payload):
 
-    # Construct the payload
-    MJFpayload = {
-        'URL'       : api_root + '/playground',
-        'api_key'   : api_key,
-        'project'   : 'sysadmin',
-        'folder'    : 'questions',
-    }
-    # Construct the list of files
-    # only add .yml files
-    all_files = MJFlist_files_in_dir(MJFdir)
-    push_files = []
-    for a_file in all_files:
-        if a_file[-4:] == '.yml':
-            push_files.append(a_file)
-    MJFpayload['files'] = push_files
+    # Add URL and api key to the payload
+    payload['URL'] = config_data['api_root'] + '/playground'
+    payload['key'] = config_data['api_key']
+    payload['project'] = project_name
 
-    # Send
-    MJFresponse = MJFpush_to_playground(MJFpayload)
-    if MJFresponse.status_code == 204:
-        print("All good!")
-    else:
-        print("Error: ", MJFresponse.text)
+# clean_out_playground
+# Expect: Nothing
+# Return: Nothing
+# Deletes all files in the default project in the playground
+def clean_out_playground():
+    # Clean out the project area
+    # Need to do this folder by folder.  We get a list of what's in each
+    # folder and the delete it
+    
+    for folder in project_folders:
+        payload = {}
+        payload['folder'] = folder
+        the_files = MJFlist_playground_files(payload)
+        for a_file in the_files:
+            payload = {}
+            payload['folder'] = folder
+            payload['filename'] = a_file
+            MJFdelete_playground_file(payload)
 
-MJFdir = 'C:/Users/ferr0182/OneDrive - Mark Ferraretto/Software/Flinders/GitHub/docassemble-sysadmin/docassemble/sysadmin/data/questions'
-#MJFdir = 'C:/Users/Mark/Onedrive - Mark Ferraretto/Software/Flinders/GitHub/flinders_scripts'
+# Main Program
+def main():
+    global project_name
+
+    # Process command-line arguments
+    parser = argparse.ArgumentParser(description='Push local code to playground')
+    parser.add_argument('path_to_package', help='Path to the top of docassemble package (eg: /path/to/docassemble-packagename)')
+    parser.add_argument('--project', '-p', help='Docassemble playground project name (default is {})'.format(project_name))
+
+    args = parser.parse_args()
+
+    # Store variables
+    da_package = args.path_to_package
+    if args.project is not None:
+        project_name = args.project.strip()
+    
+    #Initialise
+    get_API_key()
+    get_API_URL()
+    clean_out_playground()
+
+    # We assume that the path supplied and now in da_package is of the format 
+    # /path/to/docassemble-packagename
+    # We need to construct the package name and we do some basic sanity checking
+    # as well.
+    # Get the last part of the path
+    packagename = os.path.basename(os.path.normpath(da_package))
+    # trim off the leading 'docassemble-'
+    packagename = packagename.replace('docassemble-', '')
+
+    # Construct the path to the four folders
+    path_to_folders = os.path.join(da_package, 'docassemble/{}/data'.format(packagename))
+
+    # Now we can push files up.  We only push up stated folders
+    for a_folder in project_folders:
+        MJFpayload = {}
+        MJFpayload['folder'] = a_folder
+
+        # Construct the list of files and add to payload
+        folder_path = os.path.join(path_to_folders, a_folder)
+        MJFpayload['files'] = MJFlist_files_in_dir(folder_path)
+
+        # Send
+        MJFresponse = MJFpush_to_playground(MJFpayload)
+        if MJFresponse.ok:
+            print('Pushed {}'.format(path_to_folders))
+        else:
+            print("Error: pushing {} to package {}: {} {} {} ".format(a_folder, packagename, MJFresponse.status_code, MJFresponse.reason, MJFresponse.text))
 main()
---
+++ git_manipulator.py
@@ -0,0 +1,110 @@
+from github import Github
+import git
+import os.path
+import csv
+
+# Global variables
+g          = None    # PyGithub object
+# Destination path for cloning assessments
+clone_path = "C:/Users/ferr0182/Flinders/Flinders Law - LLAW3301 Law in a Digital Age/_LLAW3301_2020_S1/Assessment C3" 
+# CSV Export of user data from FLO
+user_data = "C:/Users/ferr0182/Flinders/Flinders Law - LLAW3301 Law in a Digital Age/_LLAW3301_2020_S1/llaw3301-law-in-a-digital-age---2020-s1 20200408.csv"
+
+def initialise():
+    # Expect: Nothing
+    # Return: Nothing
+    # Sets up our initial connections and populates global variables
+
+    # Connect to my GH
+    global g
+    g = Github("cb83f61b559649196081f9678399adba277a7dca")
+
+def fork_repo(FAN):
+    # Expect: FAN
+    # Return: Repository object of forked repository
+    # Constructs the path to the repository on GH, being
+    # <FAN>/docassemble-<FAN>
+
+    path_to_repo = '{}/docassemble-{}'.format(FAN, FAN)
+    repo_to_fork = g.get_repo(path_to_repo)
+    return g.get_user().create_fork(repo_to_fork)
+
+def clone_repo(forked_repo, group):
+    # Expect: Repository object containing clone information,
+    #         Project group identifier
+    # Return: Nothing
+    # TODO: Return true/false on cloning?
+    # Clones the repo into a subdir in the project dir
+
+    # Make sure we have a group.
+    if not group:
+        raise "Group must have a value"
+
+    project_dir = os.path.join(clone_path, group)
+    # Make the directory
+    try:
+        os.mkdir(project_dir)
+    except FileExistsError as e:
+        # We don't care if the directory exits and is a directory
+        if os.path.isdir(project_dir):
+            pass
+        else:
+            raise e
+    except Exception as e:
+        raise e
+
+    # Clone into directory.
+    clone_URL = forked_repo.html_url
+    clone_dir = clone_URL.split('/')[-1]
+    # We don't need to store the resut of the clone_from() call but
+    # we do just in case we want it in future and also helps with
+    # debugging.
+    cloned_repo = git.Repo.clone_from(clone_URL, os.path.join(project_dir, clone_dir))
+
+    
+def process_csv():
+    # Expect: Nothing
+    # Return: dictionary containing csv data we need
+    # Opens CSV and returns a dictionary of FANs and project groups
+    csvfile = open(user_data)
+    reader = csv.DictReader(csvfile)
+
+    result = {}
+    for row in reader:
+        FAN = row['FAN']
+        result[FAN] = None
+        # Search through the Groups until we find a group containing a
+        # a project group.  We assume only project groups start with
+        # 'P'
+        for key in row.keys():
+            try:
+                if key[0:5] == 'Group' and row[key][0] == 'P':
+                    result[FAN] = row[key]
+                    break
+            except IndexError as e:
+                # Catch index out of range errors
+                pass
+
+    csvfile.close()
+    return result
+
+#
+# Main Program
+#
+def main():
+    initialise()
+
+    # Get list of students and project groups
+    all_data = process_csv()
+
+    # For each student we fork their repo and clone to our path
+    for FAN in all_data.keys():
+        try:
+            forked_repo = fork_repo(FAN)
+            clone_repo(forked_repo, all_data[FAN])
+        except Exception as e:
+            print('Error processing FAN: {} group {}: {}'.format(FAN, FAN, str(e)))
+
+
+if __name__ == "__main__":
+    main()
